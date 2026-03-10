a716edd98faa113493560400a71b84f80a2948e2
DriesOpalfvens <147134914+DriesOpalfvens@users.noreply.github.com>
2026-03-10 09:26:40 +0100

    1

--- StratenProject_Groep/StratenProjectBL/Domein/Province.cs
+++ StratenProject_Groep/StratenProjectBL/Domein/Province.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 
 public class Province {
-    public global::System.Int32 MyProperty { get; set; }
-
-public Dic MyProperty { get; set; }
-public D MunicipalityId { get; set; }
-public int ProvinceId { get; set; }
-public string ProvinceName { get; set; }
+    public  Dictionary<int, Municapality> MunicipalityIDs { get; set; }
+    public int ProvinceId { get; set; }
+    public string ProvinceName { get; set; }
 
     public Province() {
-    
+        ProvinceId = id
     }
 }
