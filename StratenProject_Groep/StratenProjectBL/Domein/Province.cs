78ace7bf3c9a75847a99d199a9bf21f97093b541
DriesOpalfvens <147134914+DriesOpalfvens@users.noreply.github.com>
2026-03-10 09:40:57 +0100

    Province class

--- StratenProject_Groep/StratenProjectBL/Domein/Province.cs
+++ StratenProject_Groep/StratenProjectBL/Domein/Province.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using System.Globalization;
 
 public class Province {
-    public  Dictionary<int, Municapality> MunicipalityIDs { get; set; }
+    public Province(int id, string name) {
+        ProvinceId = id;
+        ProvinceName = name;
+    }
     public int ProvinceId { get; set; }
     public string ProvinceName { get; set; }
+    private Dictionary<int, Municapality> Municipalities = new Dictionary<int, Municapality>;
 
-    public Province() {
-        ProvinceId = id
+    public void AddMunicipality(Municapality municapality) {
+        Municipalities.TryAdd(municapality.Id, municapality);   
     }
+
+    public bool HasMunicpality(int id) { 
+        return Municipalities.ContainsKey(id);
+    }
+
+    public Municapality GetMunicapality(int id) {
+        return Municipalities[id];
+    }
+    public IReadOnlyList<Municapality> GetMunicapalities() { 
+        return Municipalities.Values.ToList(); 
+    }
+
 }
