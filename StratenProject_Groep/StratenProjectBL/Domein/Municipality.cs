5e731d57525ea06b67cc8c83aa6ed43f53848760
DriesOpalfvens <147134914+DriesOpalfvens@users.noreply.github.com>
2026-03-10 09:23:14 +0100

    Initial upload 2

---
+++ StratenProject_Groep/StratenProjectBL/Domein/Municipality.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Municapality {
+
+    public Municapality(int id) {
+
+        Id = id;
+
+    }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    private SortedSet<string> StreetNames = new SortedSet<string>();
+
+
+    public void AddStreetName(string streetname) {
+
+        StreetNames.Add(streetname);
+
+    }
+
+    public IReadOnlyList<string> GetStreetNames() {
+
+        return StreetNames;
+
+    }
+
+}
