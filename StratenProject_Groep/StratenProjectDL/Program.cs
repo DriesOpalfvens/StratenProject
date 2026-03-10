4dbc4f460282dfe6fddd0d1ed52f7de14687a4e4
matysin-sen <238651134+matysin-sen@users.noreply.github.com>
2026-03-10 10:11:11 +0100

    de datalaag aangevuld met al de readers maar die is nog leeg

--- StratenProject_Groep/StratenProjectDL/Program.cs
+++ StratenProject_Groep/StratenProjectDL/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-           
+           public List<Province> ReadFiles(Dictionary<string,string> filesMap,string dir)
+        {
+            Dictionary<int, Province> provinces = new Dictionary<int, Province>();
+            Dictionary<int, Municapality> municapalities = new Dictionary<int, Municapality>();
+            Dictionary<int,string> streetNames = new Dictionary<int,string>();
+
+            // lees provintie id
+            HashSet<int>provinceIds = new HashSet<int>();
+            using (StreamReader p = new StreamReader(Path.Combine(dir, filesMap["provinces"])))
+            {
+                string[] ids = p.ReadLine().Trim().Split(",");
+                foreach (string id in ids)
+                {
+                    provinceIds.Add(Int32.Parse(id));
+                }
+            }
+
+            // lees de provincienamen + provincieId+gemeenteId
+            using (StreamReader gp = new StreamReader(Path.Combine(dir, filesMap["link_Province_MunicipalityNames"])))
+            {
+
+            }
+
+
+            // lees gemeentenamen + gemeenteid
+            using (StreamReader g = new StreamReader(Path.Combine(dir, filesMap["municipalityNames"])))
+            {
+
+            }
+
+            // lees straatamen
+            using (StreamReader s = new StreamReader(Path.Combine(dir, filesMap["streetNames"])))
+            {
+
+            }
+
+            // lees straatnaamId + gemeenteId
+            using (StreamReader sg = new StreamReader(Path.Combine(dir, filesMap["link_StreetName_Municipality"]))) { 
+            
+
+            }
+
+            return provinces.Values.ToList();
+
         }
     }
 }
